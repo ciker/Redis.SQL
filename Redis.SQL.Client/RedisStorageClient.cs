841ee31bcbabd168f7e85f13af82487ded62b19d
asarnaout <asarnaout@hotmail.com>
2017-09-04 11:52:08 +0200

    Adding functions for redis sets

--- Redis.SQL.Client/RedisStorageClient.cs
+++ Redis.SQL.Client/RedisStorageClient.cs
@@ -19,6 +19,7 @@ namespace Redis.SQL.Client
             _redisDatabase = RedisConnectionMultiplexer.GetMultiplexer().Connection.GetDatabase(databaseIndex);
         }
 
+        #region Strings
         public async Task<T> GetValue<T>(string key)
         {
             var value = await _redisDatabase.StringGetAsync(key.ToLower());
@@ -29,16 +30,31 @@ namespace Redis.SQL.Client
         {
             return await _redisDatabase.StringSetAsync(key.ToLower(), JsonConvert.SerializeObject(value));
         }
+        #endregion
 
-        public async Task<bool> StoreHashField<T>(string hashSet, string key, T value)
-        {
-            return await _redisDatabase.HashSetAsync(hashSet.ToLower(), key.ToLower(), JsonConvert.SerializeObject(value));
-        }
-        
+        #region HashSets
         public async Task<T> GetHashField<T>(string hashSet, string key)
         {
             var value = await _redisDatabase.HashGetAsync(hashSet.ToLower(), key.ToLower());
             return string.IsNullOrEmpty(value) ? default(T) : JsonConvert.DeserializeObject<T>(value);
         }
+
+        public async Task<bool> StoreHashField<T>(string hashSet, string key, T value)
+        {
+            return await _redisDatabase.HashSetAsync(hashSet.ToLower(), key.ToLower(), JsonConvert.SerializeObject(value));
+        }
+        #endregion
+
+        #region Sets
+        public async Task<bool> SetContains<T>(string key, T value)
+        {
+            return await _redisDatabase.SetContainsAsync(key.ToLower(), JsonConvert.SerializeObject(value));
+        }
+
+        public async Task<bool> AddToSet<T>(string key, T value)
+        {
+            return await _redisDatabase.SetAddAsync(key.ToLower(), JsonConvert.SerializeObject(value));
+        }
+        #endregion
     }
 }
