94d990c04b4155149680b7473d20bbdb61ea2092
asarnaout <asarnaout@hotmail.com>
2017-09-04 13:43:30 +0200

    Implementing a storage interface in the RedisStorageClient

---
+++ Redis.SQL.Client/Interfaces/IStorageClient.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+
+namespace Redis.SQL.Client.Interfaces
+{
+    internal interface IStorageClient
+    {
+        Task<T> GetValue<T>(string key);
+        Task<bool> StoreValue<T>(string key, T value);
+        Task<T> GetHashField<T>(string hashSet, string key);
+        Task<bool> StoreHashField<T>(string hashSet, string key, T value);
+        Task<bool> SetContains<T>(string key, T value);
+        Task<bool> AddToSet<T>(string key, T value);
+    }
+}
--- Redis.SQL.Client/RedisStorageClient.cs
+++ Redis.SQL.Client/RedisStorageClient.cs
@@ -1,10 +1,11 @@
 ﻿using System.Threading.Tasks;
 using Newtonsoft.Json;
+using Redis.SQL.Client.Interfaces;
 using StackExchange.Redis;
 
 namespace Redis.SQL.Client
 {
-    internal class RedisStorageClient
+    internal class RedisStorageClient : IStorageClient
     {
         private readonly IDatabase _redisDatabase;
 
@@ -20,38 +21,38 @@ namespace Redis.SQL.Client
         }
 
         #region Strings
-        internal async Task<T> GetValue<T>(string key)
+        public async Task<T> GetValue<T>(string key)
         {
             var value = await _redisDatabase.StringGetAsync(key.ToLower());
             return string.IsNullOrEmpty(value) ? default(T) : JsonConvert.DeserializeObject<T>(value);
         }
 
-        internal async Task<bool> StoreValue<T>(string key, T value)
+        public async Task<bool> StoreValue<T>(string key, T value)
         {
             return await _redisDatabase.StringSetAsync(key.ToLower(), JsonConvert.SerializeObject(value));
         }
         #endregion
 
         #region HashSets
-        internal async Task<T> GetHashField<T>(string hashSet, string key)
+        public async Task<T> GetHashField<T>(string hashSet, string key)
         {
             var value = await _redisDatabase.HashGetAsync(hashSet.ToLower(), key.ToLower());
             return string.IsNullOrEmpty(value) ? default(T) : JsonConvert.DeserializeObject<T>(value);
         }
 
-        internal async Task<bool> StoreHashField<T>(string hashSet, string key, T value)
+        public async Task<bool> StoreHashField<T>(string hashSet, string key, T value)
         {
             return await _redisDatabase.HashSetAsync(hashSet.ToLower(), key.ToLower(), JsonConvert.SerializeObject(value));
         }
         #endregion
 
         #region Sets
-        internal async Task<bool> SetContains<T>(string key, T value)
+        public async Task<bool> SetContains<T>(string key, T value)
         {
             return await _redisDatabase.SetContainsAsync(key.ToLower(), JsonConvert.SerializeObject(value));
         }
 
-        internal async Task<bool> AddToSet<T>(string key, T value)
+        public async Task<bool> AddToSet<T>(string key, T value)
         {
             return await _redisDatabase.SetAddAsync(key.ToLower(), JsonConvert.SerializeObject(value));
         }
