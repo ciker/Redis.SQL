529b66ddb4a169ddb030e9efce96e332b3d9534b
asarnaout <asarnaout@hotmail.com>
2017-09-04 03:15:57 +0200

    Adding a singleton encapsulating the connection multiplexer

---
+++ Redis.SQL.Client/RedisConnectionMultiplexer.cs
@@ -0,0 +1,23 @@
+﻿using StackExchange.Redis;
+
+namespace Redis.SQL.Client
+{
+    internal sealed class RedisConnectionMultiplexer
+    {
+        public static readonly string RedisConnectionKey = "RedisConnectionKey";
+
+        private static RedisConnectionMultiplexer _multiplexer;
+
+        internal readonly ConnectionMultiplexer Connection;
+
+        private RedisConnectionMultiplexer()
+        {
+            Connection = ConnectionMultiplexer.Connect(RedisConnectionKey);
+        }
+
+        internal RedisConnectionMultiplexer GetMultiplexer()
+        {
+            return _multiplexer ?? (_multiplexer = new RedisConnectionMultiplexer());
+        }
+    }
+}
