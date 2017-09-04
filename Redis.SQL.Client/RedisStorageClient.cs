0685496b1ad5cfb5258b7a9c63db14f3e41df458
asarnaout <asarnaout@hotmail.com>
2017-09-04 11:55:16 +0200

    Changing the accessibility of the Redis storage client

--- Redis.SQL.Client/RedisStorageClient.cs
+++ Redis.SQL.Client/RedisStorageClient.cs
@@ -4,11 +4,11 @@ using StackExchange.Redis;
 
 namespace Redis.SQL.Client
 {
-    public class RedisStorageClient
+    internal class RedisStorageClient
     {
         private readonly IDatabase _redisDatabase;
 
-        public RedisStorageClient()
+        internal RedisStorageClient()
         {
             var configurationManager = new ConfigurationManager();
 
@@ -20,38 +20,38 @@ namespace Redis.SQL.Client
         }
 
         #region Strings
-        public async Task<T> GetValue<T>(string key)
+        internal async Task<T> GetValue<T>(string key)
         {
             var value = await _redisDatabase.StringGetAsync(key.ToLower());
             return string.IsNullOrEmpty(value) ? default(T) : JsonConvert.DeserializeObject<T>(value);
         }
 
-        public async Task<bool> StoreValue<T>(string key, T value)
+        internal async Task<bool> StoreValue<T>(string key, T value)
         {
             return await _redisDatabase.StringSetAsync(key.ToLower(), JsonConvert.SerializeObject(value));
         }
         #endregion
 
         #region HashSets
-        public async Task<T> GetHashField<T>(string hashSet, string key)
+        internal async Task<T> GetHashField<T>(string hashSet, string key)
         {
             var value = await _redisDatabase.HashGetAsync(hashSet.ToLower(), key.ToLower());
             return string.IsNullOrEmpty(value) ? default(T) : JsonConvert.DeserializeObject<T>(value);
         }
 
-        public async Task<bool> StoreHashField<T>(string hashSet, string key, T value)
+        internal async Task<bool> StoreHashField<T>(string hashSet, string key, T value)
         {
             return await _redisDatabase.HashSetAsync(hashSet.ToLower(), key.ToLower(), JsonConvert.SerializeObject(value));
         }
         #endregion
 
         #region Sets
-        public async Task<bool> SetContains<T>(string key, T value)
+        internal async Task<bool> SetContains<T>(string key, T value)
         {
             return await _redisDatabase.SetContainsAsync(key.ToLower(), JsonConvert.SerializeObject(value));
         }
 
-        public async Task<bool> AddToSet<T>(string key, T value)
+        internal async Task<bool> AddToSet<T>(string key, T value)
         {
             return await _redisDatabase.SetAddAsync(key.ToLower(), JsonConvert.SerializeObject(value));
         }
